7eb09325bda762b9387b56b798b11bec4709a7b0
Ashraful Islam <ashraful@field.buzz>
2017-08-29 16:04:52 +0600

    Added Dashboard Component

---
+++ src/components/Dashboard.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class Dashboard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <h1 className="text-center">Welcome Dashboard</h1>
+        )
+    }
+}
+
+export default Dashboard;
