9dbe9243a2d36564f4a9a000fdf4b6afe6f19dc8
Ashraful Islam <ashrafulrobin3@gmail.com>
2017-10-20 00:50:42 +0600

    Added a simple filter from backend (Need to update UI)

--- src/components/ActivityLog.js
+++ src/components/ActivityLog.js
@@ -1,18 +1,38 @@
 import React, {Component} from "react";
 import BasicStore from "../stores/basic-store";
 import LogListView from "./LogListView";
+import {withRouter} from "react-router-dom";
+import qs from "query-string";
 
 class ActivityLog extends Component {
     constructor(props) {
         super(props);
         this.state = {
             loading: true,
-            logs: []
+            logs: [],
+            q: "",
         };
+        this.testFilter = this.testFilter.bind(this);
+        this.testInputChange = this.testInputChange.bind(this);
+    }
+
+    testInputChange(e) {
+        const val = e.target.value;
+        this.setState({q: val});
+        this.props.history.push("?q=" + val);
+        e.preventDefault();
+    }
+
+    testFilter(e) {
+        const parsed = qs.parse(this.props.location.search);
+        this.setState({q: parsed.q});
+        this.getActivityLog();
+        e.preventDefault();
     }
 
     getActivityLog() {
-        const url = BasicStore.makeUrl('api/v1/pms/activity-logs/');
+        const parsed = qs.parse(this.props.location.search);
+        const url = BasicStore.makeUrl('api/v1/pms/activity-logs/?q=' + parsed.q);
         const payload = {
             method: 'GET',
             headers: BasicStore.headers
@@ -22,7 +42,6 @@ class ActivityLog extends Component {
         }).then((data) => {
             // set loading false for stop loading feature
             this.setState({loading: false, logs: data.results});
-            console.log(this.state.logs);
         }).catch((err) => {
             console.log(err);
         });
@@ -36,10 +55,14 @@ class ActivityLog extends Component {
         return (
             <div className="container-fluid">
                 <h1>Activities...</h1>
+                <div>
+                    <input type="text" onChange={this.testInputChange}/>
+                    <button className="m-1 btn btn-xs btn-outline-danger" onClick={this.testFilter}>Update</button>
+                </div>
                 <LogListView logs={this.state.logs} loading={this.state.loading}/>
             </div>
         )
     }
 }
 
-export default ActivityLog
+export default withRouter(ActivityLog);
