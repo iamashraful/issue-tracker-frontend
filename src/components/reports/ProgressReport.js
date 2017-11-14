fd1175f6447654c89a1d0209735b540291bc5021
Ashraful Islam <ashraful@field.buzz>
2017-11-14 15:03:49 +0600

    Added API calling method to get data from server

--- src/components/reports/ProgressReport.js
+++ src/components/reports/ProgressReport.js
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import {Line, Pie} from 'react-chartjs-2';
+import BasicStore from "../../stores/basic-store";
 
 
 class ProgressReport extends Component {
@@ -7,11 +8,12 @@ class ProgressReport extends Component {
         super(props);
         this.state = {
             loading: true,
-            progressData: [0, 59, 80, 81, 56, 55, 40],
+            lineChart: [0, 59, 80, 81, 56, 55, 40],
+            reportData: "",
         };
-
+        this.staticColors = ['#EC6518', '#8ED29E', '#93357B', '#31EF0B', '#0BEF9C', '#0BC9EF', '#990BEF', '#404D05'];
         this.lineChartData = {
-            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'December'],
             datasets: [
                 {
                     label: 'Project 1',
@@ -32,28 +34,7 @@ class ProgressReport extends Component {
                     pointHoverBorderWidth: 2,
                     pointRadius: 1,
                     pointHitRadius: 10,
-                    data: [0, 59, 80, 81, 56, 55, 40]
-                },
-                {
-                    label: 'Project 2',
-                    fill: false,
-                    lineTension: 0.1,
-                    backgroundColor: 'rgba(255,192,192,0.4)',
-                    borderColor: 'rgba(255,192,192,1)',
-                    borderCapStyle: 'butt',
-                    borderDash: [],
-                    borderDashOffset: 0.0,
-                    borderJoinStyle: 'miter',
-                    pointBorderColor: 'rgba(255,192,192,1)',
-                    pointBackgroundColor: '#fff',
-                    pointBorderWidth: 1,
-                    pointHoverRadius: 5,
-                    pointHoverBackgroundColor: 'rgba(255,192,192,1)',
-                    pointHoverBorderColor: 'rgba(220,220,220,1)',
-                    pointHoverBorderWidth: 2,
-                    pointRadius: 1,
-                    pointHitRadius: 10,
-                    data: [40, 25, 55, 74, 30, 75, 40]
+                    data: this.state.lineChart
                 }
             ]
         };
@@ -78,6 +59,26 @@ class ProgressReport extends Component {
                 ]
             }]
         };
+    }
+
+    getProgressReportData() {
+        const url = BasicStore.makeUrl('api/v1/pms/progress-report/');
+        const payload = {
+            method: 'GET',
+            headers: BasicStore.headers
+        };
+        fetch(url, payload).then((response) => {
+            return response.json();
+        }).then((data) => {
+            // set loading false for stop loading feature
+            this.setState({loading: false, reportData: data});
+        }).catch((err) => {
+            console.log(err);
+        });
+    }
+
+    componentWillMount() {
+        this.getProgressReportData();
     }
 
     render() {
