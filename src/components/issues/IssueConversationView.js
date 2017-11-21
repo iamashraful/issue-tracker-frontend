57dd888b919914f0443c209e0e34281f5fc51f9d
Ashraful Islam <ashraful@field.buzz>
2017-11-21 15:19:56 +0600

    Added comment and replies view

--- src/components/issues/IssueConversationView.js
+++ src/components/issues/IssueConversationView.js
@@ -41,22 +41,73 @@ class IssueConversationView extends Component {
     }
 
     render() {
-        return (
-            <div className="modal-dialog" role="document">
+        let comment = "";
+        let CommentModalView = "";
+        if(this.state.conversation.comment !== undefined) {
+            comment = this.state.conversation.comment;
+            if (comment) {
+                CommentModalView = (
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title">Notes</h5>
+                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div className="modal-body">
+                            <div className="card">
+                                <div className="card-header p-0 pl-2">
+                                    <h4>Comment & Replies</h4>
+                                </div>
+                                <div className="card-body">
+                                    <h5 className="pl-2 mb-0">
+                                        <i className="fa fa-comments" aria-hidden="true"/>
+                                        <span className="pl-1">{comment.text}</span>
+                                    </h5>
+                                    <p className="pl-2 pt-0 text-muted">Commented by, {}</p>
+                                    <div className="ml-5">
+                                        <strong>Replies</strong>
+                                        {
+                                            comment.replies.map(reply => {
+                                                return (
+                                                    <p>
+                                                        <i className="fa fa-reply" aria-hidden="true"/>
+                                                        <span className="pl-1">{reply.text} --</span>
+                                                        <span className="text-muted">{}</span>
+                                                    </p>
+                                                )
+                                            })
+                                        }
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-danger" data-dismiss="modal">Close</button>
+                        </div>
+                    </div>
+                )
+            }
+        }
+        else {
+            CommentModalView = (
                 <div className="modal-content">
                     <div className="modal-header">
-                        <h5 className="modal-title">Conversations</h5>
+                        <h5 className="modal-title">Notes</h5>
                         <button type="button" className="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                         </button>
                     </div>
                     <div className="modal-body">
-                        <p>Modal body text goes here.</p>
-                    </div>
-                    <div className="modal-footer">
-                        <button type="button" className="btn btn-danger" data-dismiss="modal">Close</button>
+                        <p className="text-center">No notes found.</p>
                     </div>
                 </div>
+            )
+        }
+
+        return (
+            <div className="modal-dialog" role="document">
+                {CommentModalView}
             </div>
         )
     }
